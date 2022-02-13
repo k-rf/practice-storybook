ae89968780d55b08aa6a6d828f81cd3f48a462d4
Kawano Kenichi <kenichi.k.820@gmail.com>
2022-02-13 15:17:06 +0000

    :green_heart: Add mock server

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,9 +1,20 @@
-export const parameters = {
+import type { Parameters } from '@storybook/addons';
+import { initialize, mswDecorator } from 'msw-storybook-addon';
+
+import { handlers } from '../src/mocks';
+
+initialize();
+export const decorators = [mswDecorator];
+
+export const parameters: Parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
+  },
+  msw: {
+    handlers,
   },
 };
