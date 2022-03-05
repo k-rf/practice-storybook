333dcb4c12c8f4316eb1a53dd6fdcfa2e2c8f82e
Kawano Kenichi <kenichi.k.820@gmail.com>
2022-03-05 14:32:46 +0000

    :sparkles: Add config files
    環境変数を読み込むためのファイルを追加する。
    他の場所で直接 `process.env.*` を呼び出さないようにするため。
    
    MaterialUI のカスタムテーマを追加する。

---
+++ src/configs/index.ts
@@ -0,0 +1,2 @@
+export const API_URL = process.env.BASE_URL as string;
+export const NODE_ENV = process.env.NODE_ENV as string;
---
+++ src/constants/theme.ts
@@ -0,0 +1,10 @@
+import { createTheme } from '@mui/material';
+
+export const theme = createTheme({
+  palette: {
+    background: {
+      paper: 'linear-gradient(to right, #ef32d9, #89fffd)',
+      default: 'linear-gradient(to right, #ef32d9, #89fffd)',
+    },
+  },
+});
