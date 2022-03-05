662916b40009d4a829da0dd54e32f5cfac0274bf
Kawano Kenichi <kenichi.k.820@gmail.com>
2022-03-05 14:36:13 +0000

    :green_heart: Update mocks

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,7 +1,42 @@
 import { rest } from 'msw';
 
+import { sleep } from '~/utils/sleep';
+
 export const handlers = [
   rest.get('/', (req, res, ctx) => {
     return res(ctx.status(200), ctx.json({ message: 'Hello World!' }));
+  }),
+  rest.post('/auth/login', async (req, res, ctx) => {
+    await sleep(3000);
+
+    return res(
+      ctx.status(201),
+      ctx.json({
+        jwt: 'practice_storybook_token',
+        user: {
+          id: 'user',
+          email: 'john.doe@example.com',
+          firstName: 'john',
+          lastName: 'doe',
+          bio: 'bio',
+          role: 'USER',
+        },
+      })
+    );
+  }),
+  rest.get('/auth/me', async (req, res, ctx) => {
+    await sleep(3000);
+
+    return res(
+      ctx.status(200),
+      ctx.json({
+        id: 'user',
+        email: 'john.doe@example.com',
+        firstName: 'john',
+        lastName: 'doe',
+        bio: 'bio',
+        role: 'USER',
+      })
+    );
   }),
 ];
