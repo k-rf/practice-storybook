d4bf2e4f2c96cad47430257e9a737d50cf541146
Kawano Kenichi <kenichi.k.820@gmail.com>
2022-04-04 12:24:05 +0000

    :art: Update configurations

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -2,11 +2,15 @@ import type { Parameters } from '@storybook/addons';
 import { initialize, mswDecorator } from 'msw-storybook-addon';
 
 import { handlers } from '../src/mocks/handlers';
+import { AppProvider } from '../src/providers';
 
 initialize({
   onUnhandledRequest: 'bypass',
 });
-export const decorators = [mswDecorator];
+export const decorators = [
+  mswDecorator,
+  (story) => <AppProvider>{story()}</AppProvider>,
+];
 
 export const parameters: Parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
